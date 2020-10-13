05b4cb9ce628b5a51c2da40c47b91bd99903be82
Rapha <rhja@uol.com.br>
2020-10-13 02:56:10 -0300

    User + Mongoose Added Mongoose to project dependency Added an User Model and matching Mongoose User Schema and model

---
+++ src/db/user.model.mongo.ts
@@ -0,0 +1,28 @@
+import {  } from 'express';
+import mongoose, { Document, Schema } from 'mongoose';
+
+import { IUser } from '@models/user.model';
+
+type UserMongoModel = IUser & Document;
+
+const UserSchema: Schema<UserMongoModel> = new Schema<UserMongoModel>(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    }
+  }
+)
+
+const Users = mongoose.model("Users", UserSchema);
+
+export {
+  UserMongoModel, 
+  Users 
+}
+
---
+++ src/model/user.model.ts
@@ -0,0 +1,6 @@
+export interface IUser 
+{
+  username: string;
+  password: string;
+  // maybe more, not for this exercise
+}
