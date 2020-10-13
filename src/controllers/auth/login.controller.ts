8a7bac861f67e201927c3016ea480122af870086
Rapha <rhja@uol.com.br>
2020-10-13 15:16:10 -0300

    user signup Endpoint + storage of hashed user data in DB

--- src/controllers/auth/login.controller.ts
+++ src/controllers/auth/login.controller.ts
@@ -2,6 +2,7 @@ import { Request, Response, NextFunction } from "express";
 import bcrypt, {  } from "bcrypt"
 
 import { IUser } from "@models/user.model";
+import { Users } from "@src/db/user.model.mongo";
 
 export const login = (req: Request, res: Response, next: NextFunction) =>
 {
@@ -10,8 +11,48 @@ export const login = (req: Request, res: Response, next: NextFunction) =>
 
 export const signup = (req: Request, res: Response, next: NextFunction) => 
 {
-  let user: IUser = {username: req.body.username, password: req.body.password};
-  console.log(user);
-  // bcrypt.hash()
-  res.status(200).json(user)
+  bcrypt.hash(req.body.password, 10)
+    // password hashed successfully
+    .then( 
+    (hash) => {
+      const user: IUser = {username: req.body.username, password: hash};
+
+      return Users.create(user)
+    }, 
+    // Failed to hash password
+    (reason) => {
+      return res.status(500).json({
+        meta: {
+          statusCode: 500,
+          message: reason,
+        }
+      })
+    })
+    // Document created successfully
+    .then(
+    (doc) => {
+      console.log(`User created: ${doc}`);
+
+      return res.status(201).json({
+        meta: {
+          statusCode: 201,
+          message: "User created successfully",
+        }
+      })
+    },
+    // Document couldn't be created
+    (reason) => {
+      // Determine Error to give usefull message
+      var message: string = "Unkown Error";
+
+      if (reason.code === 11000){
+        message = "User already exists";
+      }
+      return res.status(400).json({
+        meta: {
+          statusCode: 400,
+          message: message,
+        }
+      })
+    })
 }
